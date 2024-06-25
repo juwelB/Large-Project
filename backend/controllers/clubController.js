7aed4de2619171dd2ab62903ffd45ba337933e00
Jovanny Vasquez <jovannyvasquez15@gmail.com>
2024-06-24 21:32:05 -0400

    Created createClub method

--- backend/controllers/clubController.js
+++ backend/controllers/clubController.js
@@ -1,1 +1,46 @@
 //Controller for club-related routes
+
+// import mongoose
+const mongoose = require('mongoose');
+
+// import axios
+const axios = require('axios');
+
+// import the club model
+const Club = require('../models/club');
+
+// import express
+const express = require('express');
+
+// import dotenv for environment variables
+require('dotenv').config();
+
+// create a new club
+const createClub = async (req, res) => {
+    try {
+        // create a new club
+        const club = new Club({
+            name: req.body.name,
+            clubInfo: {
+                industry: req.body.industry,
+                description: req.body.description
+            },
+            adminId: req.body.adminId
+        });
+
+        // save the club
+        const savedClub = await club.save();
+
+        // return the saved club
+        res.json(savedClub);
+    } 
+    catch (err) {
+        // return an error if there is one
+        res.json({ message: err });
+    }
+}
+
+//Exports
+module.exports = {
+    createClub
+};
