834e18bafb4179f38885b78e4ed369d59793e9a0
Jovanny Vasquez <jovannyvasquez15@gmail.com>
2024-06-24 21:32:44 -0400

    Created rpute for createClub and POST request to create club

--- backend/routes/clubRoutes.js
+++ backend/routes/clubRoutes.js
@@ -1,1 +1,20 @@
 //Club-related routes
+
+// import express
+const express = require('express');
+
+// import the club controllers
+const clubController = require('../controllers/clubController');
+
+// create a new express router
+const {
+    createClub
+} = clubController;
+
+const router = express.Router();
+
+// POST request to create a new club
+router.post('/create', createClub);
+
+// export the router
+module.exports = router;
