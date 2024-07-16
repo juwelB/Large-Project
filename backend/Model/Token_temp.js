a764246edebcee7f4799e452ce5214ac5dd4a26a
Colin Kirby <kirbycolin26@gmail.com>
2024-07-15 23:00:03 -0400

    Add Token_temp.js to the repository

---
+++ backend/Model/Token_temp.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const tokenSchema = new Schema({
+    userId: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    token: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        expires: 3601 // Automatically delete documents after 2 hours
+    }
+});
+
+const Token = mongoose.model('Token', tokenSchema);
+
+module.exports = Token;
